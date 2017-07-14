f2330133fbaf0ff2a87b56d8fa60a24fbe4016d5
NightSky71 <george.lowe94@gmail.com>
2017-07-14 19:31:39 +0100

    Player Movement Update
    *Implements eulers equation to do movement.
    *Also a drag force for extra realism and a nice feel

--- platformer/player.js
+++ platformer/player.js
@@ -3,13 +3,13 @@ class Player {
   constructor(x, y) {
     this.position = createVector(x, y);
     this.velocity = createVector(0, 0);
-
     this.acceleration = createVector(0, 0);
     this.w = 5;
     this.h = 10;
-    this.speed = 5;
+    this.speed = 50;
 
     this.mass = 10;
+    this.dragFactor = 0.2;
     this.dragForce = createVector(0, 0);
   }
 
@@ -18,22 +18,38 @@ class Player {
   }
 
   move() {
+    this.acceleration.x = this.acceleration.y = 0;
+    
     if (keyIsDown(LEFT_ARROW)) {
-      this.position.x -= this.speed;
+      this.acceleration.x -= this.speed / this.mass;
     }
 
     if (keyIsDown(RIGHT_ARROW)) {
-      this.position.x += this.speed;
+      this.acceleration.x += this.speed / this.mass;
     }
 
     if (keyIsDown(UP_ARROW)) {
-      this.position.y -= this.speed;
+      this.acceleration.y -= this.speed / this.mass;
     }
 
     if (keyIsDown(DOWN_ARROW)) {
-      this.position.y += this.speed;
+      this.acceleration.y += this.speed / this.mass;
     }
-
+    
+    // Equation of motions
+    this.dragForce.x = this.velocity.x * this.dragFactor; 
+    this.dragForce.y = this.velocity.y * this.dragFactor;
+    
+    this.acceleration.x -= this.dragForce.x;
+    this.acceleration.y -= this.dragForce.y;
+    
+    this.velocity.x += this.acceleration.x;
+    this.velocity.y += this.acceleration.y;
+    
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    
+    // If the player goes offscreen wrap around
     this.position.x = (this.position.x + width)  % width;
     this.position.y = (this.position.y + height) % height;
   }

37e0d6a9018605df997fc59ffbdede615fadd3e4
James Forsyth <forsythjames@hotmail.co.uk>
2017-07-19 19:52:52 +0100

    Added ability to create and destroy world tiles on the fly

--- platformer/world.js
+++ platformer/world.js
@@ -28,6 +28,10 @@ class World {
       sandstone: [3,0],
       concrete: [4,0]
     };
+
+    // Dirty flag to keep track of changes made to the buffer
+    // Dirty will be set when a tile is created or destroyed, prompting pixel loads and updates.
+    this.dirty = false;
   }
 
   /*
@@ -35,23 +39,20 @@ class World {
    */
   load() {
     this.tileSet = loadImage('tiles/tileset.png');
+
+    // By default, the image will be transparent black
     this.tileNone = createImage(this.tileSizeDrawn, this.tileSizeDrawn);
-    this.tileNone.loadPixels();
-    this.tileNone.pixels.fill(0);
-    this.tileNone.updatePixels();
+    /*this.tileNone.loadPixels();
+    this.tileNone.pixels.fill(255);
+    this.tileNone.updatePixels();*/
   }
 
   /*
    *  Initialise the world image using tiles
    */
   init() {
-    // Create a buffer to draw the tiles to.
+    // Create a buffer to draw the tiles to. It will be transparent black by default
     this.buffer = createGraphics(width, height);
-
-    // Set background to transparent
-    this.buffer.loadPixels();
-    this.buffer.pixels.fill(0);
-    this.buffer.updatePixels();
 
     // The defines which tiles go where and is smaller than the canvas
     this.mapWidth = ceil(width / this.tileSizeDrawn);
@@ -63,31 +64,27 @@ class World {
 
     for (let i = 0; i < this.mapHeight; i++) {
       for (let j = 0; j < this.mapWidth; j++) {
+        // Make some noise
         let n = this.generateNoise(j, i);
+
+        // Store the noise in a map
         this.noiseMap[j + i * this.mapWidth] = n;
 
         // Get the index of the tile on the tileset
         // null will be returned for a none tile, so no drawing should be done.
         let index = this.noiseToTileIndex(n);
 
-        if (index) {
-          this.groundMap[j + i * this.mapWidth] = true;
-
-          // Convert the index to pixel coordinates on the tileset
-          let coord = this.tileIndexToCoord(index);
-
-          this.buffer.image(this.tileSet,
-                            this.tileSizeDrawn * j,
-                            this.tileSizeDrawn * i,
-                            this.tileSizeDrawn,
-                            this.tileSizeDrawn,
-                            coord[0],
-                            coord[1],
-                            this.tileSizeActual,
-                            this.tileSizeActual);
-        }
+        // Create the specified tile
+        this.createTile(index, i, j);
       }
     }
+  }
+
+  /*
+   *  Uses Perlin noise to determine which tile to place in a given tile coordinate.
+   */
+  generateNoise(i, j) {
+    return noise(i * this.frequency, j * this.frequency);
   }
 
   /*
@@ -110,10 +107,106 @@ class World {
   }
 
   /*
-   *  Uses Perlin noise to determine which tile to place in a given tile coordinate.
+   *  Draws a given tile index (e.g. world.tileIndex.clay) at row i, column j in the map grid.
+   *  The function also ensures that the ground map used for collision detection is updated.
    */
-  generateNoise(i, j) {
-    return noise(i * this.frequency, j * this.frequency);
+  createTile(index, i, j) {
+    // Don't draw none or undefined tiles
+    if (index === null) {
+      return;
+    }
+
+    // Update dirty pixels before drawing to the buffer
+    if (this.dirty) {
+      this.buffer.updatePixels();
+      this.dirty = false;
+    }
+
+    this.groundMap[j + i * this.mapWidth] = true;
+
+    // Convert the index to pixel coordinates on the tileset
+    let coord = this.tileIndexToCoord(index);
+
+    this.buffer.image(this.tileSet,
+                      this.tileSizeDrawn * j,
+                      this.tileSizeDrawn * i,
+                      this.tileSizeDrawn,
+                      this.tileSizeDrawn,
+                      coord[0],
+                      coord[1],
+                      this.tileSizeActual,
+                      this.tileSizeActual);
+  }
+
+  /*
+   *  Destroys the tile at row i, column j in the map grid, and removes it from the ground collision
+   *  detection map.
+   */
+  destroyTile(i, j) {
+    // This method will make the buffer dirty since it needs to load pixels
+    if (!this.dirty) {
+      this.dirty = true;
+      this.buffer.loadPixels();
+    }
+
+    this.groundMap[j + i * this.mapWidth] = false;
+
+    // Manually set the alpha channel of pixels to transparent. This seems a bit overkill, but replace
+    // blending a transparent image over the top doesn't seem to work.
+    for (let y = i * this.tileSizeDrawn; y < (i + 1) * this.tileSizeDrawn; y++) {
+      for (let x = j * this.tileSizeDrawn; x < (j + 1) * this.tileSizeDrawn; x++) {
+        this.buffer.pixels[3 + 4 * (x + y * this.buffer.width)] = 0;
+      }
+    }
+
+    /*this.buffer.blendMode(REPLACE);
+    this.buffer.image(this.tileNone,
+                      this.tileSizeDrawn * j,
+                      this.tileSizeDrawn * i);*/
+
+    /*this.buffer.blend(this.tileNone,
+                      0,
+                      0,
+                      this.tileSizeDrawn,
+                      this.tileSizeDrawn,
+                      this.tileSizeDrawn * j,
+                      this.tileSizeDrawn * i,
+                      this.tileSizeDrawn,
+                      this.tileSizeDrawn,
+                      REPLACE);*/
+  }
+
+  /*
+   *  Destroy a batch of tiles in one go to avoid excessive pixel loads and updates
+   *  coordList should be an array of (row, col) coordinates. For example:
+   *  coordList = [
+   *    [19, 20],
+   *    [20, 19],
+   *    [20, 20],
+   *    [20, 21],
+   *    [21, 20]
+   *  ]
+   */
+  destroyTiles(coordList) {
+    if (!this.dirty) {
+      this.dirty = true;
+      this.buffer.loadPixels();
+    }
+
+    for (let [i, j] of coordList) {
+        // Remove from collision detection
+        this.groundMap[j + i * this.mapWidth] = false;
+
+        // Manually set the alpha channel of pixels to transparent. This seems a bit overkill, but
+        // replace blending a transparent image over the top doesn't seem to work.
+        for (let y = i * this.tileSizeDrawn; y < (i + 1) * this.tileSizeDrawn; y++) {
+          for (let x = j * this.tileSizeDrawn; x < (j + 1) * this.tileSizeDrawn; x++) {
+            this.buffer.pixels[3 + 4 * (x + y * this.buffer.width)] = 0;
+          }
+        }
+    }
+
+    this.buffer.updatePixels();
   }
 
   /*
@@ -136,6 +229,12 @@ class World {
    *  Draws the underlying generated image.
    */
   draw() {
+    if (this.dirty) {
+      // Update pixels if any changes were made
+      this.buffer.updatePixels();
+      this.dirty = false;
+    }
+
     background(230, 250, 250);
     image(this.buffer, 0, 0, width, height);
   }
